4885a536229a5b3e606f76be2dc5c97cadf480de
Gijoe614 <jvandoorn@students.mchenry.edu>
2016-08-28 12:47:50 -0500

    Add files via upload
    madlibs-1

---
+++ madlibs1.py
@@ -0,0 +1,33 @@
+# Variables
+person = " Barrack Obama"
+pet = "monkey"
+color = "brown"
+example = "wood"
+place = "white house"
+group = "congressmen"
+person2 = " Joe biden"
+adverb = "angirly"
+action = "dances"
+
+# poem
+print(person+ " had a little " + pet + ",")
+print("His fleece was " + color + " as " + example + ",")
+print("And everywhere that " + person + " went,")
+print("The " + pet + " was sure to go.\n")
+
+print("He followed him to " + place + " one day,")
+print("Which was against the rule,")
+print("It made the " + group + " laugh and play")
+print("to see a " + pet + " at the " + place + ".\n")
+
+print("And so the " + person2 + " turned it out,")
+print("But still it lingered near,")
+print("And waited " + adverb + " about,")
+print("Till " + person + " did appear.\n")
+
+print("Why does the " + pet + " love " + person + " so?")
+print("The eager " + group + " cry.")
+print("Why, " + person +" " + action + " the " + pet + ", you know,")
+print("The " + person2 + " did reply.")
+
+
